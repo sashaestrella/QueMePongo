610d6037b3e67a3587d68067ea14b83576a9c5b9
sashaestrella <sestrella@intranet.certant.com>
2021-05-24 12:53:37 -0300

    tambien vuelvo a crear el Atuendo, y ahora un Guardarropa

---
+++ src/main/java/main/Atuendo.java
@@ -0,0 +1,38 @@
+package main;
+
+public class Atuendo {
+    Prenda prendaSuperior;
+    Prenda prendaInferior;
+    Prenda calzado;
+    Prenda accesorio;
+    private Guardarropa guardarropaAsociado;
+    private boolean rechazado;
+
+    public Atuendo(Prenda prendaSuperior, Prenda prendaInferior, Prenda calzado, Prenda accesorio){
+        this.prendaSuperior = prendaSuperior;
+        this.prendaInferior = prendaInferior;
+        this.calzado = calzado;
+        this.accesorio = accesorio;
+        this.rechazado = false;
+    }
+
+    public void setGuardarropa(Guardarropa guardarropa) {
+        this.guardarropaAsociado = guardarropa;
+    }
+
+    public Guardarropa getGuardarropaAsociado() {
+        return this.guardarropaAsociado;
+    }
+
+    public void aceptarAtuendo() {
+        this.rechazado = false;
+    }
+
+    public void rechazarAtuendo() {
+        this.rechazado = true;
+    }
+
+    public boolean estaRechazado() {
+        return this.rechazado;
+    }
+}
---
+++ src/main/java/main/Guardarropa.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Guardarropa {
+    private List<Prenda> prendas;
+    private List<Atuendo> atuendosAceptados;
+    private List<Atuendo> atuendosRechazados;
+
+    public List<Prenda> getPrendas() {
+        return prendas;
+    }
+
+    public List<Atuendo> getAtuendosAceptados() {
+        return this.atuendosAceptados;
+    }
+
+    public List<Atuendo> getAtuendosRechazados(){
+        return this.atuendosRechazados;
+    }
+
+    public void setPrendas(List<Prenda> prendas) {
+        this.prendas = prendas;
+    }
+
+    public void agregarPrenda(Prenda nuevaPrenda) {
+        this.prendas.add(nuevaPrenda);
+    }
+
+    private List<Prenda> filtrarPorTipoDePrenda(TipoPrenda tipo) {
+        return this.getPrendas().stream().filter(prenda -> prenda.esDeTipoPrenda(tipo)).collect(Collectors.toList());
+    }
+
+    public void agregarAListaDeAceptados(Atuendo atuendo) {
+        sacarDeRechazado(atuendo);
+        this.atuendosAceptados.add(atuendo);
+    }
+
+    public void agregarAListaDeRechazados(Atuendo atuendo) {
+        sacarDeAceptado(atuendo);
+        this.atuendosRechazados.add(atuendo);
+    }
+
+    public void sacarDeAceptado(Atuendo atuendo){
+        if(this.getAtuendosAceptados().stream().anyMatch(atuendoAceptado -> atuendoAceptado.equals(atuendo))){
+            eliminarAtuendoAceptado(this.getAtuendosAceptados().stream().filter(atuendoAceptado -> atuendoAceptado.equals(atuendo)).collect(Collectors.toList()).get(0));
+        }
+    }
+
+    public void sacarDeRechazado(Atuendo atuendo){
+        if(this.getAtuendosRechazados().stream().anyMatch(atuendoRechazado -> atuendoRechazado.equals(atuendo))){
+            eliminarAtuendoRechazado(this.getAtuendosRechazados().stream().filter(atuendoRechazado -> atuendoRechazado.equals(atuendo)).collect(Collectors.toList()).get(0));
+        }
+    }
+
+    public void eliminarAtuendoAceptado(Atuendo atuendo) {
+        this.atuendosAceptados.remove(atuendo);
+    }
+
+    public void eliminarAtuendoRechazado(Atuendo atuendo) {
+        this.atuendosRechazados.remove(atuendo);
+    }
+
+}
